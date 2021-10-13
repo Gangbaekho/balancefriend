8fea7a2c32e840e440166f15032328ec9a81a703
roorooro1 <roorooro9933@gmail.com>
2021-10-13 18:28:33 +0900

    add custom exception handling

--- src/main/java/com/example/balancefriend/exception/ExceptionHandlingController.java
+++ src/main/java/com/example/balancefriend/exception/ExceptionHandlingController.java
@@ -20,11 +20,30 @@ public class ExceptionHandlingController {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
     }
 
+    @ExceptionHandler(CUserAlreadyExistException.class)
+    public ResponseEntity<?> handleCUserAlreadyExistException (CUserAlreadyExistException e){
 
+        Map<String,String> message = new HashMap<>();
+        message.put("message",e.getMessage());
 
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
+    }
 
+    @ExceptionHandler(CTodoNotFoundException.class)
+    public ResponseEntity<?> handleCTodoNotFoundException (CTodoNotFoundException e){
 
+        Map<String,String> message = new HashMap<>();
+        message.put("message",e.getMessage());
 
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
+    }
 
+    @ExceptionHandler(CUserAuthorizationException.class)
+    public ResponseEntity<?> handleCUserAuthorizationException (CUserAuthorizationException e){
 
+        Map<String,String> message = new HashMap<>();
+        message.put("message",e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(message);
+    }
 }
