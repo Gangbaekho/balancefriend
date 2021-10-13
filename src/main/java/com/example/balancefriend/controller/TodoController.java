2ce77d8d66f4a88e252fc3a695dfe23a86a41c6f
roorooro1 <roorooro9933@gmail.com>
2021-10-13 18:20:13 +0900

    add todo controller

---
+++ src/main/java/com/example/balancefriend/controller/TodoController.java
@@ -0,0 +1,60 @@
+package com.example.balancefriend.controller;
+
+import com.example.balancefriend.dto.*;
+import com.example.balancefriend.security.CurrentUser;
+import com.example.balancefriend.security.UserPrincipal;
+import com.example.balancefriend.service.TodoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/todos")
+@RestController
+public class TodoController {
+
+    private final TodoService todoService;
+
+    @GetMapping("/{todoId}/full")
+    public TodoGetFullResponseDto getSingleFullTodo(@CurrentUser UserPrincipal userPrincipal, @PathVariable(value = "todoId") Long todoId){
+
+        return todoService.getSingleFullTodo(userPrincipal.getId(), todoId);
+    }
+
+    @GetMapping("/list/full")
+    public List<TodoGetFullResponseDto> getListFullTodos(@CurrentUser UserPrincipal userPrincipal){
+
+        return todoService.getListFullTodo(userPrincipal.getId());
+    }
+
+    @GetMapping("/{todoId}/partial")
+    public TodoGetPartialResponseDto getSinglePartialTodo(@CurrentUser UserPrincipal userPrincipal, @PathVariable(value = "todoId") Long todoId){
+
+        return todoService.getSinglePartialTodo(userPrincipal.getId(), todoId);
+    }
+
+    @GetMapping("/list/partial")
+    public List<TodoGetPartialResponseDto> getListPartialTodos(@CurrentUser UserPrincipal userPrincipal){
+
+        return todoService.getListPartialTodos(userPrincipal.getId());
+    }
+
+    @PostMapping("")
+    public TodoCreateResponseDto createTodo(@CurrentUser UserPrincipal userPrincipal, @RequestBody TodoCreateRequestDto requestDto){
+
+        return todoService.createTodo(userPrincipal.getId(),requestDto);
+    }
+
+    @PutMapping("")
+    public TodoUpdateResponseDto updateTodo(@CurrentUser UserPrincipal userPrincipal, @RequestBody TodoUpdateRequestDto requestDto){
+
+        return todoService.updateTodo(userPrincipal.getId(),requestDto);
+    }
+
+    @DeleteMapping("/{todoId}")
+    public TodoDeleteResponseDto deleteTodo(@CurrentUser UserPrincipal userPrincipal, @PathVariable(value = "todoId") Long todoId){
+
+        return todoService.deleteTodo(userPrincipal.getId(),todoId);
+    }
+}
